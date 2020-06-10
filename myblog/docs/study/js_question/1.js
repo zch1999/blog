908a8a93d8c5c80fe034e0606dabb4f958343434
zch1999 <1901394767@qq.com>
2020-06-10 14:48:20 +0800

    正常流

--- myblog/docs/study/js_question/1.js
+++ myblog/docs/study/js_question/1.js
@@ -1,27 +1,35 @@
-new Promise((resolve, reject) => {
-  console.log("promise1")
-  resolve()
-})
-.then(() => {
-  console.log("then11")
-  new Promise((resolve, reject) => {
-    console.log("promise2")
-    resolve()
-  }) 
-  .then(() => {
-    console.log("then21")
-    new Promise((resolve, reject) => {
-      console.log("promise3")
-      resolve()
-    }).then(() => {
-      console.log("then31")
-    }).then(() => {
-      console.log("then32")
-    })
-  })
-  .then(() => {
-    console.log("then22")
-  })
-}).then(() => {
-  console.log("then12")
-})
+// new Promise((resolve, reject) => {
+//   console.log("promise1")
+//   resolve()
+// })
+// .then(() => {
+//   console.log("then11")
+//   new Promise((resolve, reject) => {
+//     console.log("promise2")
+//     resolve()
+//   }) 
+//   .then(() => {
+//     console.log("then21")
+//     new Promise((resolve, reject) => {
+//       console.log("promise3")
+//       resolve()
+//     }).then(() => {
+//       console.log("then31")
+//     }).then(() => {
+//       console.log("then32")
+//     })
+//   })
+//   .then(() => {
+//     console.log("then22")
+//   })
+// }).then(() => {
+//   console.log("then12")
+// })
+
+
+for(var i=0; i<3; i++) {
+  (function(i) {
+  setTimeout(() => {
+      console.log(i)
+  },2000)})(i)
+}
